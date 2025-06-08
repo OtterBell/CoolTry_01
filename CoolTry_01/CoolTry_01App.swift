4e7a07bde3419b3873e09703e67e5c212e7532c5
OtterBell <stu9987765@gmail.com>
2025-06-08 22:15:55 +0800

    Initial Commit

---
+++ CoolTry_01/CoolTry_01App.swift
@@ -0,0 +1,17 @@
+//
+//  CoolTry_01App.swift
+//  CoolTry_01
+//
+//  Created by 莊惠淇 on 2025/6/8.
+//
+
+import SwiftUI
+
+@main
+struct CoolTry_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
